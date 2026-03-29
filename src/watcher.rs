9429c69dced746b136576a56ac39b028e2d12379
Justin Pallansch <pallanschja@gmail.com>
2026-03-28 20:48:20 -0400

    Added paths to output

--- src/watcher.rs
+++ src/watcher.rs
@@ -14,10 +14,12 @@ use uuid::Uuid;
 
 use crate::config::{EventFlags, WatchCommands, WatchItem, YamlChoice};
 
+type WatchEvent = (Uuid, Vec<PathBuf>);
+
 pub struct WatchFiles {
     watch_map: HashMap<Uuid, ActiveWatcher>,
-    sx: Sender<Uuid>,
-    rx: Receiver<Uuid>,
+    sx: Sender<WatchEvent>,
+    rx: Receiver<WatchEvent>,
 }
 
 impl WatchFiles {
@@ -37,12 +39,13 @@ impl WatchFiles {
         }
 
         loop {
-            if let Ok(id) = self.rx.recv_timeout(Duration::from_secs(1)) {
+            if let Ok((id, paths)) = self.rx.recv_timeout(Duration::from_secs(1)) {
                 // Definitely exists
                 let watch = self.watch_map.get_mut(&id).unwrap();
                 let now = Instant::now();
 
                 watch.last_send = Some(now);
+                watch.files.extend(paths);
 
                 trace!("Event for '{}'", watch.name);
 
@@ -62,6 +65,7 @@ impl WatchFiles {
                     }
 
                     watcher.last_send = None;
+                    watcher.files.clear();
                 }
             }
         }
@@ -103,6 +107,7 @@ impl Default for WatchFiles {
 pub struct ActiveWatcher {
     pub name: String,
     pub cmd: String,
+    pub files: HashSet<PathBuf>,
     last_send: Option<Instant>,
     debounce: u64,
 }
@@ -115,6 +120,7 @@ impl ActiveWatcher {
         Self {
             cmd,
             name,
+            files: HashSet::new(),
             last_send: None,
             debounce: item.debounce,
         }
@@ -124,11 +130,11 @@ impl ActiveWatcher {
 pub struct WatchEventHandler {
     pub id: Uuid,
     pub flags: EventFlags,
-    pub sx: Sender<Uuid>,
+    pub sx: Sender<WatchEvent>,
 }
 
 impl WatchEventHandler {
-    pub fn new(id: Uuid, flags: EventFlags, sx: Sender<Uuid>) -> Self {
+    pub fn new(id: Uuid, flags: EventFlags, sx: Sender<WatchEvent>) -> Self {
         Self { id, flags, sx }
     }
 }
@@ -136,11 +142,11 @@ impl WatchEventHandler {
 impl EventHandler for WatchEventHandler {
     fn handle_event(&mut self, event: notify::Result<Event>) {
         match event {
-            Ok(Event { kind, .. }) => {
+            Ok(Event { kind, paths, .. }) => {
                 let flag = EventFlags::from(kind);
 
                 if self.flags.intersects(flag) {
-                    self.sx.send(self.id).unwrap();
+                    self.sx.send((self.id, paths)).unwrap();
                 }
             }
             Err(e) => error!("{}", e),
@@ -188,14 +194,24 @@ fn get_single_path(item: &str, paths: &mut HashSet<PathBuf>) -> Result<()> {
 }
 
 fn run_command(watcher: &ActiveWatcher) -> Result<()> {
-    let output = Command::new("sh").args(["-c", &watcher.cmd]).output()?;
+    let files = watcher
+        .files
+        .iter()
+        .map(|p| p.to_string_lossy())
+        .collect::<Vec<_>>()
+        .join(" ");
+
+    let output = Command::new("sh")
+        .args(["-c", &watcher.cmd])
+        .env("PATHS", &files)
+        .output()?;
 
     let stdout = String::from_utf8(output.stdout)?;
     let stderr = String::from_utf8(output.stderr)?;
 
     trace!(
-        "\nNAME: '{}'\nSTDOUT:\n{}\nSTDERR:\n{}",
-        &watcher.name, stdout, stderr
+        "\nNAME: '{}'\nPATHS: {}\nSTDOUT:\n{}\nSTDERR:\n{}",
+        &watcher.name, files, stdout, stderr
     );
 
     Ok(())
